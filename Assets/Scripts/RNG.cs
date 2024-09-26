b211c689ed56e5d2b1ebde57c7d4269ece3beee5
HP-GADGET\13472 <dillon.smith@my/liu.edu>
2024-09-26 18:12:42 -0400

    More stuff working

--- Assets/Scripts/RNG.cs
+++ Assets/Scripts/RNG.cs
@@ -8,13 +8,17 @@ public class RNG : MonoBehaviour
     public int x, y, b;
     public GameObject X, Y;
 
+    public Sprite[] numsprites;
+
     // Start is called before the first frame update
     void Start()
     {
         x = Random.Range(1, 9);
         y = Random.Range(1, 9);
         b = x + y;
-        Debug.Log(x +"+"+ y + "=" + b);       
+        Debug.Log(x +"+"+ y + "=" + b);
+        X.GetComponent<SpriteRenderer>().sprite = numsprites[x];
+        Y.GetComponent<SpriteRenderer>().sprite = numsprites[y];
     }
 
     // Update is called once per frame
